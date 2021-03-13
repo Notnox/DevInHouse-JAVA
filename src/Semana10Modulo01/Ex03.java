f85065e0d41d353757a5b2d98b15d86161fab40a
LucasPires <74467549+Notnox@users.noreply.github.com>
2021-03-13 14:22:09 -0300

    [SP10] Exercício 3

---
+++ src/Semana10Modulo01/Ex03.java
@@ -0,0 +1,14 @@
+package Semana10Modulo01;
+
+import java.util.Random;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		Random numeroAleatorio = new Random();
+		
+		System.out.println("Número aleatorio entre 0 - 1: " + numeroAleatorio.nextDouble());
+
+	}
+
+}
