98c0ab2a4c98762bbc6e17e9bccab18d3274cf06
LucasPires <74467549+Notnox@users.noreply.github.com>
2021-03-26 14:07:12 -0300

    [SP14] Exercício 8

---
+++ Semana02Modulo02Ex08/src/br/com/devinhouse/banco/contas/Conta.java
@@ -0,0 +1,10 @@
+package br.com.devinhouse.banco.contas;
+
+public class Conta {
+	private static int numeroDeContas;
+	private String nome;
+	private String sobreNome;
+	
+	
+	
+}
---
+++ Semana02Modulo02Ex08/src/br/com/devinhouse/banco/funcionarios/Gerente.java
@@ -0,0 +1,5 @@
+package br.com.devinhouse.banco.funcionarios;
+
+public class Gerente {
+
+}
