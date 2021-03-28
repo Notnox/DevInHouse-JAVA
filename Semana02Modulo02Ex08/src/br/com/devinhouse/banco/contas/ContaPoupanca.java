a59909dcd538a388ca2f397e13b3579060a0b724
LucasPires <74467549+Notnox@users.noreply.github.com>
2021-03-28 08:35:59 -0300

    [SP14] Exercício 13

---
+++ Semana02Modulo02Ex08/src/br/com/devinhouse/banco/contas/ContaPoupanca.java
@@ -0,0 +1,14 @@
+package br.com.devinhouse.banco.contas;
+
+public class ContaPoupanca extends Conta {
+	
+	public ContaPoupanca (String nome, String sobreNome, double saldo) {
+		super(nome, sobreNome, saldo);
+	}
+	
+	public void renderJurosDePoupanca() {
+		double rendimento = super.getSaldo() * 0.01;
+		
+		super.depositar(rendimento);
+	}
+}
--- Semana02Modulo02Ex08/src/br/com/devinhouse/banco/main/Main.java
+++ Semana02Modulo02Ex08/src/br/com/devinhouse/banco/main/Main.java
@@ -2,22 +2,22 @@ package br.com.devinhouse.banco.main;
 
 import br.com.devinhouse.banco.bonificacao.CalcularBonificacao;
 import br.com.devinhouse.banco.contas.ContaCorrente;
+import br.com.devinhouse.banco.contas.ContaPoupanca;
 import br.com.devinhouse.banco.funcionarios.GerenteRegional;
 
 public class Main {
 
 	public static void main(String[] args) {
-		GerenteRegional novoGerente = new GerenteRegional("Lucas", "Pires", "12312", 2300, "12312", 2);
+		 ContaPoupanca novaContaPoupanca = new ContaPoupanca("Xis", "De", 0);
+		 
+		 novaContaPoupanca.depositar(100);
+		 
+		 System.out.println(novaContaPoupanca.getSaldo());
+		 
+		 novaContaPoupanca.renderJurosDePoupanca();
+		 
+		 System.out.println(novaContaPoupanca.getSaldo());
 		
-		System.out.println(CalcularBonificacao.bonificacao(novoGerente));
-		
-		ContaCorrente novaContaCorrente = new ContaCorrente("lulu", "pipi", 0);
-		
-		novaContaCorrente.depositar(1000);
-		
-		novaContaCorrente.sacar(100);
-		
-		System.out.println(novaContaCorrente.getSaldo());
 	}
 
 }
