da14992659576f34c225f4c7a093fca79fdd21b8
LucasPires <74467549+Notnox@users.noreply.github.com>
2021-04-01 15:20:23 -0300

    [SP15] Exercício 12

---
+++ Semana03Modulo02_JAVA/[SP15] Exercício 12/src/br/com/devinhouse/empresa/main/Main.java
@@ -0,0 +1,30 @@
+package br.com.devinhouse.empresa.main;
+
+import br.com.devinhouse.empresa.folhadepagamento.Funcionario;
+import br.com.devinhouse.empresa.folhadepagamento.FuncionarioAssalariado;
+import br.com.devinhouse.empresa.folhadepagamento.FuncionarioComissionado;
+import br.com.devinhouse.empresa.folhadepagamento.FuncionarioHorista;
+
+public class Main {
+
+	public static void main(String[] args) {
+		FuncionarioAssalariado funcionarioAssalariado = new FuncionarioAssalariado("João", "Mendes", 15000);
+		FuncionarioComissionado funcionarioComissionado = new FuncionarioComissionado("Bruno", "Ferronato", .75, 6000);
+		FuncionarioHorista funcionarioHorista = new FuncionarioHorista("Brayan", "Moncks", 1500, 4);
+		
+		Funcionario arrayFuncionarios[] = new Funcionario[Funcionario.getTotalDeFuncionarios()];
+		
+		arrayFuncionarios[0] = funcionarioAssalariado;
+		arrayFuncionarios[1] = funcionarioComissionado;
+		arrayFuncionarios[2] = funcionarioHorista;
+		
+		double valorFolha = 0;
+		
+		for(Funcionario func: arrayFuncionarios) {
+			System.out.println(func);
+			valorFolha += func.calcularSalarioMensal();
+		}
+		System.out.println("Folha de pagamento: R$" + valorFolha);
+	}
+
+}
