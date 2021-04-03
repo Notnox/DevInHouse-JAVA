9cdcb17cadb5d0baa8823cd97ef6080b151302be
LucasPires <74467549+Notnox@users.noreply.github.com>
2021-04-03 16:03:43 -0300

    [SP16] Exercício 9

---
+++ Semana04Modulo02_JAVA/[SP16] Exercício 9/src/br/com/devinhouse/batalhafinal/enums/Arma.java
@@ -0,0 +1,5 @@
+package br.com.devinhouse.batalhafinal.enums;
+
+public enum Arma {
+	ESPADA, MACHADO, MARTELO, CLAVA, ARCO, BESTA, CAJADO, LIVRO_DE_MAGIAS, ARMADILHA, MACHADO_DUPLO;
+}
---
+++ Semana04Modulo02_JAVA/[SP16] Exercício 9/src/br/com/devinhouse/batalhafinal/interfaces/Atacante.java
@@ -0,0 +1,6 @@
+package br.com.devinhouse.batalhafinal.interfaces;
+
+public interface Atacante {
+
+	void atacar(Object alvo);
+}
