dde083bc05122e32d7cfa0de4e909778fccfa015
LucasPires <74467549+Notnox@users.noreply.github.com>
2021-04-11 09:43:50 -0300

    [SP17] - Exercício 2 - Servlet

---
+++ Semana05Modulo02_JAVA/[SP17] Exercício 2 - Servlet/br.com.devinhouse.tabuada/Java Resource/src/br/com/devinhouse/tabuada/Tabuada.java
@@ -0,0 +1,29 @@
+package br.com.devinhouse.tabuada;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Tabuada extends HttpServlet {
+
+	private int numero = 5;
+	protected void service (HttpServletRequest request,HttpServletResponse response)
+            throws ServletException, IOException {
+
+        PrintWriter out = response.getWriter();
+
+        // escreve o texto
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>Tabuada do número " + numero + "</h1>");
+        for (int x = 1; x < 11; x++) {
+        	 out.println("<h2>" + numero + " + " + x + " = " + numero * x + "</h2>");
+        }
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
